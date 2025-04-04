96baaf8b9678ea4eca56172ee487d3138f3d719f
Angelina <linakirsanova@gmail.com>
2025-04-04 14:34:21 +0100

    Add migrations

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,6 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+migrate = Migrate(app, db, render_as_batch=True)
 
 class NameForm(FlaskForm):
   name = StringField("What is your name?", validators=[DataRequired()])
