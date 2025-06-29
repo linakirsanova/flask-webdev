770f3364c087dfecc7061d29bbf7517849954ca1
Angelina <linakirsanova@gmail.com>
2025-06-29 15:33:31 +0300

    Use werkzeug to hash user's passwords, add tests

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from app.models import User
 from flask import current_app
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def test_app_creation():
     app = create_app('testing')
@@ -16,3 +17,14 @@ def test_database_insert(new_app):
     u = User(email='john@example.com', username='john')
     db.session.add(u)
     db.session.commit()
+
+def test_user_password(new_app):
+    u1 = User(email='victor@example.com', username='victor', password = 'corn')
+    u2 = User(email='anna@example.com', username='anna', password = 'clown')
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.commit()
+    assert u1.password_hash
+    assert check_password_hash(u1.password_hash, 'corn')
+    assert not check_password_hash(u1.password_hash, 'flower')
+    assert u1.password_hash != u2.password_hash
