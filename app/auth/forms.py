7d4e0864e251cc5da55316c9811cba64f3676800
Angelina <linakirsanova@gmail.com>
2025-07-21 14:59:28 +0100

    Create a registration form for new users

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Regexp, EqualTo, ValidationError, Length
+from ..models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Length(1,64),
+                                    Email()])
+    username = StringField('Username', validators=[
+        DataRequired(),
+        Length(1, 64),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                   'Usernames must have only letters, numbers, dots, or underscores',
+        )])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        EqualTo('password_confirm', message='Passwords do not match.'
+        )])
+    password_confirm = PasswordField('Password (confirm):',
+                                     validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Sorry! Username already in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from . import auth
 from flask_login import logout_user, login_user, login_required, current_user
-from werkzeug.security import check_password_hash
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,16 +17,36 @@ def login():
     if user and user.verify_password(form.password.data):
       login_user(user, remember=form.remember_me.data)
       flash('Login successful!', 'success')
-      return redirect(url_for('main.index'))
+      next = request.args.get('next')
+      if next is None or not next.startswith('/'):
+        next = url_for('main.index')
+      return redirect(next)
   else:
     flash('Invalid email or password', 'error')
-
   return render_template('auth/login.html', form=form)
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-  return render_template('register.html')
+  if current_user.is_authenticated:
+     return redirect(url_for('main.index'))
+
+  form = RegistrationForm()
+
+  if form.validate_on_submit():
+    user = User(
+      email=form.email.data,
+      username=form.username.data,
+      password=form.password.data
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    flash('Registration successful! You can now login.', 'success')
+    return redirect(url_for('auth.login'))
+
+  return render_template('auth/register.html', form=form)
 
 @auth.route('/logout')
 @login_required
